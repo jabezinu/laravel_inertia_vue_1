65812746af00b6b6240685a20148215e13bd6cf2
jabezinu <https://github.com/jabezinu>
2025-08-09 04:18:05 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+// Route::resource('todos', TodoController::class)->except(['create', 'show', 'edit']);
+
+Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
+Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+Route::put('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
+Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
