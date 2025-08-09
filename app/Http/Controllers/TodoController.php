d011f727936e02fabb8bd11c044d630b8b222f98
jabezinu <https://github.com/jabezinu>
2025-08-09 03:07:40 +0300

    todo controller

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $todos = Todo::latest()->get();
+        
+        return Inertia::render('Todos/Index', [
+            'todos' => $todos
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string'
+        ]);
+
+        Todo::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'completed' => false
+        ]);
+
+        return redirect()->route('todos.index');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'completed' => 'boolean'
+        ]);
+
+        $todo->update($request->only(['title', 'description', 'completed']));
+
+        return redirect()->route('todos.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return redirect()->route('todos.index');
+    }
+}
